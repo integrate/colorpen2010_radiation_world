cdc3deb8439b36cde76bc35ce2a3193e1b4ecd2c
colorpen2010 <melnik.olga.22.11.1981@gmail.com>
2021-12-03 19:04:21 +0200

    version 1.1

--- radiation_world_main.py
+++ radiation_world_main.py
@@ -1,18 +1,34 @@
 import wrap
-#world
+
+# world
+wrap.add_sprite_dir("sprite's")
 wrap.world.create_world(1920, 1080)
 wrap.world.set_title('2 player game')
 wrap.world.set_back_image("sprite's/world/ground-texture_(32).jpg")
-#geroy_1
-wrap.add_sprite_dir("sprite's")
-geroy=wrap.sprite.add("human's",1920/2,1080/2,'geroy1')
+semla = wrap.sprite.add('world', 500, 900, 'lol')
+# geroy_1
+geroy = wrap.sprite.add("human's", 1920 / 2, 1080 / 2, 'geroy1')
+
 
 @wrap.on_mouse_move()
-def geroy_move(pos_x,pos_y):
-    wrap.sprite.set_angle_to_point(geroy,pos_x,pos_y)
+def geroy_move(pos_x, pos_y):
+    wrap.sprite.set_angle_to_point(geroy, pos_x, pos_y)
+
+
+def sdvin_mir():
+    gerx = wrap.sprite.get_x(geroy)
+    gery = wrap.sprite.get_y(geroy)
+    yholpox = gerx - 960
+    yholpoy = gery - 540
+
+    wrap.sprite.move(semla, -yholpox, -yholpoy)
+    wrap.sprite.move(geroy, -yholpox, -yholpoy)
+
+
 @wrap.on_key_always(wrap.K_w)
-def geroy_move_w(keys,control_keys):
+def geroy_move_w(keys, control_keys):
     if wrap.K_w in keys and not wrap.KMOD_SHIFT in control_keys:
-        wrap.sprite.move_at_angle_dir(geroy,5)
+        wrap.sprite.move_at_angle_dir(geroy, 5)
     elif wrap.KMOD_SHIFT in control_keys:
-        wrap.sprite.move_at_angle_dir(geroy,10)
+        wrap.sprite.move_at_angle_dir(geroy, 10)
+    sdvin_mir()
