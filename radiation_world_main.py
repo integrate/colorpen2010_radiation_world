227765108c425b014ca64c01673979519b6b15bd
colorpen2010 <melnik-olgaaa@ukr.net>
2021-12-17 19:01:38 +0200

    pyki BETA

--- radiation_world_main.py
+++ radiation_world_main.py
@@ -6,18 +6,22 @@ import wrap,random
 wrap.add_sprite_dir("sprite's")
 wrap.world.create_world(1920, 1080)
 wrap.world.set_title('2 player game')
+wrag2 = []
 wrap.world.set_back_image("sprite's/world/ground-texture_(32).jpg")
 semla = wrap.sprite.add('world', 500, 900, 'lol')
 # geroy_1
 geroy = wrap.sprite.add("human's", 1920 / 2, 1080 / 2, 'geroy1')
-@wrap.always(1000)
+@wrap.always(20000)
 def wrag():
-    global wrag2
     wrag1=wrap.sprite.add('wrag_enemy', random.randint(0, 1920), random.randint(0, 1080))
-    wrag2=[wrag1]
     wrag2.append(wrag1)
 
-
+@wrap.on_mouse_down(wrap.BUTTON_LEFT)
+def zastrelu(pos_x, pos_y):
+    global avtomat_potron
+    avtomat_potron=wrap.sprite.add('pyli',1920/2,1080/2)
+    wrap.sprite.set_angle(avtomat_potron,wrap.sprite.get_angle(geroy))
+    wrap.sprite.move_at_angle_point()
 
 
 @wrap.on_mouse_move()
