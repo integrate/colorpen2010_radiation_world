1f1079c947238ae995f6e16da1857b06877ceff7
colorpen2010 <melnik-olgaaa@ukr.net>
2021-12-24 19:07:05 +0200

    version 1.4

--- radiation_world_main.py
+++ radiation_world_main.py
@@ -9,24 +9,41 @@ spisoc_potronov=[]
 wrap.world.set_back_image("sprite's/world/ground-texture_(32).jpg")
 semla = wrap.sprite.add('world', 500, 900, 'lol')
 # geroy_1
-geroy = wrap.sprite.add("human's", 1920 / 2, 1080 / 2, 'geroy1')
+geroy = wrap.sprite.add("human's", 1920 / 2, 1080 / 2, 'geroy1pylimet')
 
 
-@wrap.always(20000)
+@wrap.always(10000)
 def wrag():
     wrag1 = wrap.sprite.add('wrag_enemy', random.randint(0, 1920), random.randint(0, 1080))
     wrag2.append(wrag1)
 
+@wrap.always(100)
+def wrag_napal():
+    for u in wrag2:
+        wrap.sprite.move_at_angle_point(u,960,540,3)
+
+def otkinyl_wraga():
+    for y in wrag2:
+        for t in spisoc_potronov:
+            hleb=wrap.sprite.is_collide_sprite(y,t)
+            if hleb==True:
+               wrap.sprite.move_at_angle(y,wrap.sprite.get_angle(t),5)
+
 
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def zastrelu(pos_x, pos_y):
-    global avtomat_potron
     avtomat_potron = wrap.sprite.add('pyli', 960, 540)
     wrap.sprite.set_angle(avtomat_potron, wrap.sprite.get_angle(geroy))
     what = wrap.sprite.get_angle(geroy)
     wrap.sprite.move_at_angle(avtomat_potron, what, 45)
     spisoc_potronov.append(avtomat_potron)
-    wrap.sprite.move_at_angle_dir(avtomat_potron,random.randint(50,200))
+    print(avtomat_potron)
+
+@wrap.always
+def sleep():
+    for i in spisoc_potronov:
+        wrap.sprite.move_at_angle_dir(i,5)
+    otkinyl_wraga()
 
 
 @wrap.on_mouse_move()
